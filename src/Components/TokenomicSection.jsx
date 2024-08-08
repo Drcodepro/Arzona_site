fadeb519cbefb8bc43f614606fd4ff4a1aa25e81
Drcodepro <drrahangdale01@gmail.com>
2024-08-08 12:07:57 +0530

    neww

--- src/Components/TokenomicSection.jsx
+++ src/Components/TokenomicSection.jsx
@@ -1,5 +1,6 @@
 import "./All_css/TokenomicSection.css";
 import tokenomic_graph from "../assets/images/Arizona_Tokenomic_Img.webp";
+import bscScan_logo from "../assets/icons/bscScan_logo.png"
 import { useEffect, useState } from "react";
 
 export default function Tokenomics() {
@@ -12,6 +13,7 @@ export default function Tokenomics() {
 
   const [copied, setCopied] = useState(false);
   const text = "0x9a1C69D8277489B74C8C93671Dd1c1d17Dda802A";
+  
 
   const handleCopy = () => {
     navigator.clipboard
@@ -25,6 +27,16 @@ export default function Tokenomics() {
       .catch((err) => {
         console.error("Copy failed: ", err);
       });
+
+
+      // copy msg showw
+
+      const copy_msg = document.querySelector(".copy_msg");
+      copy_msg.classList.add("copy_msg_show");
+
+      setTimeout(()=>{
+        copy_msg.classList.remove("copy_msg_show");
+      },1000)
   };
 
 
@@ -91,11 +103,25 @@ export default function Tokenomics() {
       <div className="copy_contract_container">
         <h1>Contract Address</h1>
         <p>0x9a1C69D8277489B74C8C93671Dd1c1d17Dda802A</p>
+
+        <div className="contract_btn_container">
         <button onClick={handleCopy}>
           <div className="btn_content">
             Copy <i className="fa-regular fa-copy"></i>
           </div>
         </button>
+
+        <a href="">
+          <button>
+            <div className="btn_content">
+              <img src={bscScan_logo} alt="logo" className="bsc_logo" />BscScan
+            </div>
+          </button>
+        </a>
+
+        </div>
+        
+        <div className="copy_msg">Copied to ClipBoard!</div>
       </div>
     </div>
   );
